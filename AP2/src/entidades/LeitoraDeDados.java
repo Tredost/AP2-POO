d69e174374c71c993e8e14d5835fa227c4ca9c42
Esther <eympessanha@gmail.com>
2023-11-10 11:34:21 -0300

    formatei data

--- AP2/src/entidades/LeitoraDeDados.java
+++ AP2/src/entidades/LeitoraDeDados.java
@@ -39,10 +39,10 @@ public static Evento cadastrarEvento(Scanner leitor) {
     String nome = leitor.nextLine();
 
     // experimento de data
-    System.out.println("Qual a data do evento(dd.mm.yyyy)?\n");
-    String str = leitor.nextLine();
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd. MM. yyyy");
-    LocalDate data = LocalDate.parse(str, dtf);
+    System.out.println("Qual a data do evento(dd/mm/yyyy)?\n");
+    String dataStr = leitor.nextLine();
+    DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/mm/yyyy");
+    LocalDate data = LocalDate.parse(dataStr, formatador);
 
     System.out.println("Qual o local do evento?\n");
     String local = leitor.nextLine();
