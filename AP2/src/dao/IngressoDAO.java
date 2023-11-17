45035c6c2a5ba63a7c0cca832f716d1782902af1
“Ian <pk20lian@gmail.com>
2023-11-17 16:02:31 -0300

    sobrecarga ingDAO

--- AP2/src/dao/IngressoDAO.java
+++ AP2/src/dao/IngressoDAO.java
@@ -1,14 +1,38 @@
 package dao;
-import entidades.eventos.Evento;
 import entidades.ingressos.Ingresso;
+
+import java.time.LocalDate;
+import entidades.eventos.Evento;
+import entidades.eventos.Exposicao;
+import entidades.eventos.Jogo;
+import entidades.eventos.Show;
+import entidades.ingressos.IngExpo;
+import entidades.ingressos.IngShow;
+import entidades.ingressos.IngShow.EspacoEnum;
+import entidades.ingressos.IngJogo;
 import entidades.ingressos.TipoIngresso;
 
     public class IngressoDAO {
         private Ingresso ingresso;
 
 
-        //criar ingresso recebnedo os parametos da gestora, gestora puxa metodos da leitora 
+        //criar ingresso recebnedo os parametos da gestora, gestora puxa metodos da leitora
 
+
+        public String criarIngresso(Evento evento, TipoIngresso tipoIngresso, EspacoEnum espacoEnum) {
+            this.ingresso = new IngShow(evento, tipoIngresso, espacoEnum);
+            return this.ingresso.toString();
+        }
+
+        public String criarIngresso(Evento evento, TipoIngresso tipoIngresso, boolean descontoSocial) {
+            this.ingresso = new IngExpo(evento, tipoIngresso, descontoSocial);
+            return this.ingresso.toString();
+        }
+
+        public String criarIngresso(Evento evento, TipoIngresso tipoIngresso, Double descontoTorcedor) {
+            this.ingresso = new IngJogo(evento, tipoIngresso, descontoTorcedor);
+            return "Evento adicionado com sucesso!";
+        }
 
 
         public String toString() {
